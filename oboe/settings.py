a198b1a35904e896acb978ecf0f3534286257d50
Abe Levitan <allevitan@gmail.com>
2013-01-09 15:26:29 -0500

    fix my database mistake

--- oboe/settings.py
+++ oboe/settings.py
@@ -27,16 +27,16 @@ MANAGERS = ADMINS
 
 DIRNAME = os.path.dirname(__file__)
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3', # Add '', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'testdatabase',                      # Or path to database file if using sqlite3.
-#        'USER': '',                      # Not used with sqlite3.
-#        'PASSWORD': '',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add '', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'testdatabase',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 #print dj_database_url.config()
 DATABASES['default'] = dj_database_url.config()
