152bcc68362f2c4eb25ea4b4f493763353795bb1
Madison May <mmay@m.linux>
2013-01-09 16:55:11 -0500

    Commented out another reference to easy-thumbnails.

--- oboe/settings.py
+++ oboe/settings.py
@@ -42,7 +42,7 @@ DATABASES = {
 }
 
 #print dj_database_url.config()
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
