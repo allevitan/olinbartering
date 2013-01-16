a07cd003ba123811dda54f495826f1324deb6855
Madison May <madison.may@students.olin.edu>
2013-01-16 10:26:51 -0500

    Maintenance of contentview.py

--- oboe/contentview.py
+++ oboe/contentview.py
@@ -23,7 +23,11 @@ def people(request):
 		if form.is_valid():
 			cleaned_data = form.clean()
 			user_name = cleaned_data['username']
+
+			#username follows form "first.last"
 			username = '.'.join(user_name.lower().split())
+
+			#Maybe be useful in the future 
 			'''
 			filterText = cleaned_data['filters']
 			filterType = cleaned_data['filterType']
@@ -35,12 +39,17 @@ def people(request):
 			'''
 			return HttpResponseRedirect('/profile/'+username+'/')
 	else:
+		
+		#query database for list of all users and redisplay the same page.
 		users = UserData.objects.all().order_by("user")
 		form = MultiProfileDisplay()
 		filterText = "None"
 	return render(request, 'MultiProfileDisplay.html', {'users':users, 'form':form, 'filterText': filterText})
 
 def getFilter(name, helpfilter):
+	
+	#better to ask forgiveness than permission
+	#attempt to access filter object in database and create an entry if it does not exist
 	try:
 		if helpfilter:
 			bulletinFilter = Filter.objects.get(name = name, helpfilter=True)
@@ -54,80 +63,11 @@ def getFilter(name, helpfilter):
 		bulletinFilter.save()
 	return bulletinFilter
 
-def editBulletin(request):	
-	if request.method == 'POST':
-		form = BulletinForm(request.POST, request.FILES)
-		bulletin = request.session['bulletin']
-		missive = Missive.objects.filter(bulletin=bulletin).order_by("timestamp")[0]
-		if form.is_valid() and request.user.is_authenticated():
-			cleaned_data = form.clean()
-			user = User.objects.get(username = request.user)
-			userdata = user.userdata
-			email = user.email
-			subject = cleaned_data['subject']
-			message = cleaned_data['message']
-			location = cleaned_data['location']
-			relevance = cleaned_data['relevance']
-			filters = cleaned_data['filters']
-			bulletinType = cleaned_data['bulletinType']
-			bulletin.subject = subject
-			missive.message = message
-			bulletin.location = location
-			bulletin.relevance = relevance			
-			if bulletinType == "Want?":
-				price = cleaned_data['price']
-				bulletinFilter = getFilter(filters, helpfilter=False)
-				bulletin.tag = bulletinFilter
-				bulletin.helpbulletin = False
-				bulletin.price = price
-			elif bulletinType == "Help?":
-				bulletinFilter = getFilter(filters, helpfilter=True)
-				bulletin.tag = bulletinFilter
-				bulletin.helpbulletin = True
-			else:
-				raise ValidationError
-			bulletin.save()
-			missive.save()
-			return HttpResponseRedirect('/')
-	else:
-		if request.user.is_authenticated():
-			bulletin = request.session['bulletin']
-			missive = Missive.objects.filter(bulletin=bulletin).order_by("timestamp")[0]
-			data = {'subject': bulletin.subject, 'relevance':bulletin.relevance, 
-					'location':bulletin.location, 'filters': bulletin.tag.name, 'message':missive.message}
-			if bulletin.helpbulletin:
-				data['bulletinType']= 'Help?'
-			else:
-				data['bulletinType']= 'Want?'
-				data['price'] = bulletin.free
-
-			form = BulletinForm(initial = data)
-		else:
-			return HttpResponseRedirect('/login')
-	return render(request, 'editBulletin.html', {'form':form, 'edit':True})
-
-def selectBulletin(request):	
-	if request.method == 'POST':
-		bulletins = set((bulletin.subject, bulletin.subject) for bulletin in Bulletin.objects.filter(creator=request.user.userdata))
-		form = selectBulletinForm(request.POST or None, request.FILES, bulletins=bulletins)
-		if form.is_valid():
-			cleaned_data = form.clean()
-			user = User.objects.get(username = request.user)
-			userdata = user.userdata
-			subject = request.POST.get('bulletin', '')
-			bulletin = Bulletin.objects.get(subject=subject)
-			request.session['bulletin'] = bulletin
-			return HttpResponseRedirect('/editBulletin')
-	else:
-		if request.user.is_authenticated():
-			bulletins = set((bulletin.subject, bulletin.subject) for bulletin in Bulletin.objects.filter(creator=request.user.userdata))
-			form = selectBulletinForm(request.POST or None, bulletins=bulletins)
-		else:
-			return HttpResponseRedirect('/login')
-	return render(request, 'selectBulletin.html', {'form':form})
-
 def viewBulletin(request, pk):
+		
 	if pk > 0:
+		
+		#database queries for bulletin info 
 		bulletin = Bulletin.objects.get(pk=pk)
 		allreplies = Reply.objects.filter(thread__bulletin=bulletin)
 		replies= allreplies.filter(public=True).order_by("-timestamp")
@@ -143,16 +83,24 @@ def viewBulletin(request, pk):
 			else: public = False
 			user = request.user.userdata
 			if user != bulletin.creator or public:
+	
+				#send public message
 				message = cleaned_data['message']
+
+				#generate new thread if needed
 				if allreplies.filter(sender=user).exists():
 					thread = (allreplies.filter(sender=user)[0]).thread
 				else:
 					thread = Reply_Thread.objects.create(bulletin=bulletin)
 					thread.users.add(user)
 					thread.users.add(bulletin.creator)
+				
+				#save info
 				thread.save()
 				reply = Reply.objects.create(public=public, sender=user, message=message, thread=thread)
 				reply.save()
+
+	#update page
 	form = ReplyForm()
 	resolveform = ResolverCreditForm()
 	if request.user.userdata == bulletin.creator:
@@ -161,43 +109,61 @@ def viewBulletin(request, pk):
 	return render(request, 'bulletin.html', {'bulletin':bulletin, 'replies':replies, 'privatecount':privatecount, 'form':form, 'bulletinform':bulletinform, 'resolveform':resolveform})
 
 def updateBulletin(request, pk):
+	
+	#basic validation
 	if pk > 0 and request.user.is_authenticated() and request.method == 'POST':
 		bulletin = Bulletin.objects.get(pk=pk)
 	else: return HttpResponseRedirect('/home/')
+	
+	#check identity of user against identity of bulletin creator to 
+	#determine which page should be redered. 
 	if request.user.userdata == bulletin.creator:
 		form = UpdateBulletinForm(request.POST)
 		if form.is_valid():
 			cleaned_data = form.clean()
 			message = cleaned_data['missive']
+
+			#send out a new missive if the bulletin is not yet resolvd
 			if not bulletin.resolved:
 				missive = Missive.objects.create(message=message,bulletin=bulletin)
 				if request.POST.get('free',''):
 					bulletin.free = True
 				bulletin.save()
 				missive.save()
+
+			#add words of wisdom from the bulletin creator
 			elif bulletin.helpbulletin and len(message) <= 200:
 				bulletin.advice = message
 				bulletin.save()
+		
+		#re-render form with update values 
 		else: return HttpResponseRedirect('/bulletin/%d/' % bulletin.id)
+
+	#direct users who did not create the bulletin to the public bulletin page
 	else: return HttpResponseRedirect('/bulletin/%d/' % bulletin.id)
 	return HttpResponseRedirect('/home/');
 
 def filterSuggestions(request):
+
 	if request.method == "POST": 
 		form = FilterSuggestionForm(request.POST)
 		if form.is_valid:
+
+			#capture form data
 			filterName = request.POST.get('filterName', '')
-			print filterName
+
+			#send anonymous message to admins to consider updating filter list
 			subject = "New Filter: "+filterName+"?"
 			message = subject
 			send_mail(subject, message, 'allevitan@gmail.com',
 			['allevitan@gmail.com'], fail_silently=False)
 			return HttpResponseRedirect('/editProfile/')
-	else:
-		if request.user.is_authenticated():
-			form = FilterSuggestionForm()
-			return render(request, 'filterSuggestions.html', {'form': form})
-		else: HttpResponseRedirect('/login/')
+
+	#initial page render
+	if request.user.is_authenticated():
+		form = FilterSuggestionForm()
+		return render(request, 'filterSuggestions.html', {'form': form})
+	else: HttpResponseRedirect('/login/')
 		
 
 def contact(request):
@@ -211,12 +177,13 @@ def contact(request):
 			send_mail(subject, message, 'allevitan@gmail.com',
     ['allevitan@gmail.com'], fail_silently=False)
 			return HttpResponseRedirect('/')
-	else:
-		if request.user.is_active and request.user.is_authenticated():
-			data = {'emailAddress':request.user.email}
-			form = ContactForm(initial=data)
-		else:
-			form = ContactForm()
-	
-	return render(request, 'contact.html', {'form':form})
+
+	#initial page render
+	elif request.user.is_active and request.user.is_authenticated():
+		data = {'emailAddress':request.user.email}
+		form = ContactForm(initial=data)
+		return render(request, 'contact.html', {'form':form})
+
+	#redirect to login page if user is not authenticated
+	else: return HttpResponseRedirect('/login/')
 	
