f443819d9c6b42ebf6f4fa72446c17cf191dd82c
Madison May <madison.may@students.olin.edu>
2013-01-17 15:05:41 -0500

    Edit for testing incoming mail.

--- oboe/incomingmail.py
+++ oboe/incomingmail.py
@@ -1,8 +1,9 @@
 from inbound import PostmarkInbound
 import simplejson
+from django.http import HttpResponse
 
 
 def incoming(request):
 	json_data = simplejson.loads(request.raw_post_data)
-	print json_data
+	return HttpResponse(json_data)
 	inbound = PostmarkInbound(json=json_data)
