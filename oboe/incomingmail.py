6b87a8d266ac6a89693586628d45fa7ba607fd4d
Madison May <madison.may@students.olin.edu>
2013-01-21 14:44:15 -0500

    Clean code up in incomingmail.py

--- oboe/incomingmail.py
+++ oboe/incomingmail.py
@@ -7,24 +7,13 @@ import datetime
 import re
 
 @csrf_exempt
-def standard_reply(request):
+def standard_handler(request):
 	if request.method == 'POST':
 		inbound = PostmarkInbound(json = request.raw_post_data)
 		if mailinglist(inbound):
 			return mailinglist_process(inbound)
 		else:
-			return process(inbound)
-
-def resolved(subject, message):
-	regex = r'(?<!un)resolve'
-	subject_match = re.search(regex, subject, flags=re.IGNORECASE)
-	message_match = re.search(regex, message, flags=re.IGNORECASE)
-	if message_match:
-		return subject
-	if subject_match:
-		subject = re.sub(r'\[?resolved\]?[\:|\-]?[ ]?', '',  subject, flags=re.IGNORECASE)
-		return subject
-	return False
+			return reply(inbound)
 
 def mailinglist(inbound):
 	'''Check to see if email originated in carpe or helpme'''
@@ -36,6 +25,17 @@ def mailinglist_process(inbound):
 		return handle_mailing_list(False, inbound)					
 	else:
 		return handle_mailing_list(True, inbound)
+
+def resolved(subject, message):
+	regex = r'(?<!un)resolve'
+	subject_match = re.search(regex, subject, flags=re.IGNORECASE)
+	message_match = re.search(regex, message, flags=re.IGNORECASE)
+	if message_match:
+		return subject
+	if subject_match:
+		subject = re.sub(r'\[?resolved\]?[\:|\-]?[ ]?', '',  subject, flags=re.IGNORECASE)
+		return subject
+	return False
 
 def basic_info(inbound):
 	subject = inbound.subject()	
@@ -55,16 +55,39 @@ def generate_name(sender):
 	email = sender['Email']
 	return name, email
 
-def handle_mailing_list(helpfilter, inbound):
-	#Assign to mailing list
-	if helpfilter: mailing_list = 'Helpme'
-	else: mailing_list = 'Carpediem'
-	subject = inbound.subject()
+def trim_message(message):
+	regex = r'On [A-Z][a-z]{2,3}\,'
+	message_parts = re.split(regex, message, 1)
+	return message_parts[0]
+
+
+def reformat(message):
+	message_parts = re.split(r'[\_]{20,60}', message, 1)
+	message = message_parts[0]
+	return message
+
+def match_filter(subject, message, helpfilter):
+	#search subject for a matching filter if filter does not exist
+	for word in subject.split():
+		try:
+			#return tag
+			tag = Filter.objects.get(name = word.title(), helpfilter=helpfilter)
+			return tag
+		except:
+			pass
 	
-	if "Re: " in subject:
-		return send_reply(inbound, mailing_list, helpfilter)
-	else:
-		return send_bulletin(inbound, mailing_list, helpfilter)
+	#search body of email if no tag found in subject
+	message = message.lower()
+	for filterObject in Filter.objects.filter(helpfilter=helpfilter):
+		if filterObject.name.lower() in message:
+			return filterObject
+		
+	#Nothing is found, return mailing_list name as placeholder
+	if helpfilter: mailing_list = "Helpme"
+	else: mailing_list = "Carpediem"
+
+	tag = Filter.objects.get(name = mailing_list, helpfilter=helpfilter)
+	return tag
 
 def get_user(sender):
 	try:
@@ -73,7 +96,78 @@ def get_user(sender):
 		user = User.objects.get(username = '.'.join(sender['Name'].lower().split()))
 	return user
 
-def send_reply(inbound, mailing_list, helpfilter):
+def handle_mailing_list(helpfilter, inbound):
+	#Assign to mailing list
+	if helpfilter: mailing_list = 'Helpme'
+	else: mailing_list = 'Carpediem'
+	subject = inbound.subject()
+	
+	if "Re: " in subject:
+		return mailinglist_reply(inbound, mailing_list, helpfilter)
+	else:
+		return send_bulletin(inbound, mailing_list, helpfilter)
+
+def reply(inbound):
+
+	subject, sender, timestamp, message = basic_info(inbound)
+
+	#remove "Re:" from subject line
+	bulletin_subject = re.sub(r'Re\: ', '', subject)	
+
+	#remove everything but latest response
+	message = latest_response(message)
+
+	#remove replies
+	message = trim_message(message) 
+	
+	#find bulletin and reply_thread in database that match email title
+	bulletin = Bulletin.objects.get(subject__iexact = bulletin_subject)
+	
+	#check if reply_thread already exists, create one if not	
+
+	sender = inbound.sender()#need to convert to userdata instance
+		
+	if resolved(bulletin_subject, message):
+		bulletin.resolved = True
+		bulletin.save()
+		return HttpResponse('Success!')
+	
+	try:
+		#does user exist?
+		user = get_user(sender)
+
+		userdata = user.userdata
+		
+		try: 
+			reply_thread = Reply_Thread.objects.get(bulletin = bulletin)
+		except: 
+			reply_thread = Reply_Thread.objects.create(bulletin = bulletin)
+	
+		#create new reply object
+		reply = Reply.objects.create(thread = reply_thread, sender = userdata, public = False, 
+								     timestamp = timestamp, message = message)
+		reply.save()
+		reply_thread.save()
+		return HttpResponse('Success!')
+
+	except:
+		#user does not exist - generate basic info for reply
+		name, email = generate_name(sender)
+		
+		#another candidate for **kwargs
+		try: 
+			reply_thread = Reply_Thread.objects.get(bulletin = bulletin, anon_email = email, anon_name = name, anon = True)
+		except: 
+			reply_thread = Reply_Thread.objects.create(bulletin = bulletin, anon_email = email, anon_name = name, anon = True)
+
+		reply = Reply.objects.create(thread = reply_thread, anon=True, public = False, 
+								     timestamp = timestamp, message = message)
+
+		reply.save()
+		reply_thread.save()
+		return HttpResponse('Success!')
+
+def mailinglist_reply(inbound, mailing_list, helpfilter):
 	#send reply
 	subject, sender, timestamp, message = basic_info(inbound)
 
@@ -139,10 +233,42 @@ def send_reply(inbound, mailing_list, helpfilter):
 		reply_thread.save()
 		return HttpResponse('Success!')
 
-def reformat(message):
-	message_parts = re.split(r'[\_]{20,60}', message, 1)
-	message = message_parts[0]
-	return message
+def send_bulletin(inbound, mailing_list, helpfilter):
+	subject, sender, timestamp, message = basic_info(inbound)
+
+	#remove mailing_list from subject line
+	regex = r'\[' + mailing_list + '\] '
+	subject = re.sub(regex, '', subject)
+
+	#remove replies from message
+	message = trim_message(message) 
+
+	#send bulletin
+	tag = match_filter(subject, message, helpfilter)
+	relevance = datetime.datetime.now() + datetime.timedelta(1)
+
+	data = {'subject': subject, 'sender': sender, 'timestamp': timestamp, 'message': message, 
+			'tag':tag, 'relevance':relevance, 'helpfilter':helpfilter} 
+
+	print 'debugging...'
+	
+	#resolve bulletin if needed
+	if resolved(subject, message):
+		try:
+			subject = resolved(subject, message)
+			bulletin = Bulletin.objects.get(subject__iexact = subject, helpbulletin=helpfilter)
+			bulletin.resolved = True
+			bulletin.save()
+			return HttpResponse('Success!')
+		except:
+			#bulletin DNE
+			pass
+
+	#generate new bulletin
+	if helpfilter:
+		return send_help_bulletin(data)
+	else:
+		return send_want_bulletin(data)
 
 def send_help_bulletin(data): #should use **kwargs here
 	subject, sender, timestamp, message = data['subject'], data['sender'], data['timestamp'], data['message']
@@ -220,138 +346,3 @@ def send_want_bulletin(data): #should use **kwargs here
 			missive.save()
 			bulletin.save()
 			return HttpResponse('Success!')
-
-
-def trim_message(message):
-	regex = r'On [A-Z][a-z]{2,3}\,'
-	message_parts = re.split(regex, message, 1)
-	return message_parts[0]
-
-def send_bulletin(inbound, mailing_list, helpfilter):
-	subject, sender, timestamp, message = basic_info(inbound)
-
-	#remove mailing_list from subject line
-	regex = r'\[' + mailing_list + '\] '
-	subject = re.sub(regex, '', subject)
-
-	#remove replies from message
-	message = trim_message(message) 
-
-	#send bulletin
-	tag = match_filter(subject, message, helpfilter)
-	relevance = datetime.datetime.now() + datetime.timedelta(1)
-
-	data = {'subject': subject, 'sender': sender, 'timestamp': timestamp, 'message': message, 
-			'tag':tag, 'relevance':relevance, 'helpfilter':helpfilter} 
-
-	print 'debugging...'
-	
-	#resolve bulletin if needed
-	if resolved(subject, message):
-		try:
-			subject = resolved(subject, message)
-			bulletin = Bulletin.objects.get(subject__iexact = subject, helpbulletin=helpfilter)
-			bulletin.resolved = True
-			bulletin.save()
-			return HttpResponse('Success!')
-		except:
-			#bulletin DNE
-			pass
-
-	#generate new bulletin
-	if helpfilter:
-		return send_help_bulletin(data)
-	else:
-		return send_want_bulletin(data)
-			
-	
-
-def match_filter(subject, message, helpfilter):
-	#search subject for a matching filter if filter does not exist
-	for word in subject.split():
-		try:
-			#return tag
-			tag = Filter.objects.get(name = word.title(), helpfilter=helpfilter)
-			return tag
-		except:
-			pass
-	
-	#search body of email if no tag found in subject
-	message = message.lower()
-	for filterObject in Filter.objects.filter(helpfilter=helpfilter):
-		if filterObject.name.lower() in message:
-			return filterObject
-		
-	#Nothing is found, return mailing_list name as placeholder
-	if helpfilter: mailing_list = "Helpme"
-	else: mailing_list = "Carpediem"
-
-	tag = Filter.objects.get(name = mailing_list, helpfilter=helpfilter)
-	return tag
-
-def process(inbound):
-
-	subject, sender, timestamp, message = basic_info(inbound)
-
-	#remove "Re:" from subject line
-	bulletin_subject = re.sub(r'Re\: ', '', subject)	
-
-	#remove everything but latest response
-	message = latest_response(message)
-
-	#remove replies
-	message = trim_message(message) 
-	
-	#find bulletin and reply_thread in database that match email title
-	bulletin = Bulletin.objects.get(subject__iexact = bulletin_subject)
-	
-	#check if reply_thread already exists, create one if not	
-
-	sender = inbound.sender()#need to convert to userdata instance
-		
-	if resolved(bulletin_subject, message):
-		bulletin.resolved = True
-		bulletin.save()
-		return HttpResponse('Success!')
-	
-	try:
-		#does user exist?
-		user = get_user(sender)
-
-		userdata = user.userdata
-		
-		try: 
-			reply_thread = Reply_Thread.objects.get(bulletin = bulletin)
-		except: 
-			reply_thread = Reply_Thread.objects.create(bulletin = bulletin)
-	
-		#create new reply object
-		reply = Reply.objects.create(thread = reply_thread, sender = userdata, public = False, 
-								     timestamp = timestamp, message = message)
-		reply.save()
-		reply_thread.save()
-		return HttpResponse('Success!')
-
-	except:
-		#user does not exist - generate basic info for reply
-		name, email = generate_name(sender)
-		
-		#another candidate for **kwargs
-		try: 
-			reply_thread = Reply_Thread.objects.get(bulletin = bulletin, anon_email = email, anon_name = name, anon = True)
-		except: 
-			reply_thread = Reply_Thread.objects.create(bulletin = bulletin, anon_email = email, anon_name = name, anon = True)
-
-		reply = Reply.objects.create(thread = reply_thread, anon=True, public = False, 
-								     timestamp = timestamp, message = message)
-
-		reply.save()
-		reply_thread.save()
-		return HttpResponse('Success!')
-
-
-	
-				
-			
-
-
