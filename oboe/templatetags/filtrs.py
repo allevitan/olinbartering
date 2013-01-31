5767ebb570a6b85b2254fca87854c9710a271b56
Abe Levitan <allevitan@gmail.com>
2013-01-31 18:51:16 -0500

    nicer filter system

--- oboe/templatetags/filtrs.py
+++ oboe/templatetags/filtrs.py
@@ -1,10 +1,22 @@
 from django import template
-from django.template.defaultfilters import stringfilter
+from django.template import defaultfilters
 
 register = template.Library()
 
-@register.filter(name='replacebr', is_safe=True)
-@stringfilter
-def replacebr(value):
+@register.filter(name='keepbreaks', is_safe=True)
+@defaultfilters.stringfilter
+def keepbreaks(value):
+    """turns an html string into plain text, but keeps all the <br>s"""
+    value = value.replace("<br>", "\n")
+    value = defaultfilters.striptags(value)
+    return defaultfilters.linebreaksbr(value)
+
+@register.filter(name='replace')
+@defaultfilters.stringfilter
+def replace(value, args):
     """replaces all values of the first part of arg with the second, comma separated"""
-    return value.replace("<br>", "\n")
+    args = args.split(',')
+    #If they use a space after the comma
+    if args[1][0] == ' ':
+        args[1] = args[1][1:]
+    return value.replace(args[0], args[1])
